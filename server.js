eab72bcb551e66d0480110f3fb1c16960eb813eb
StefanVelkoski <velkoskis@gmail.com>
2021-02-16 12:45:35 +0100

    initial

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 
 
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'build')));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + 'build','index.html'));
 });
 
 app.listen(port);
