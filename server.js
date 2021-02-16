7f6ebcaade55e2390525eedc4044098b9188fad2
StefanVelkoski <velkoskis@gmail.com>
2021-02-16 12:26:07 +0100

    initial

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const port =  process.env.PORT || 8080;
 
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + 'blog-loka-fe/build/favicon.ico'));
 
 
 app.use(express.static(__dirname));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + 'build','index.html'));
+    res.sendFile(path.join(__dirname + 'blog-loka-fe/build/index.html'));
 });
 
 app.listen(port);
